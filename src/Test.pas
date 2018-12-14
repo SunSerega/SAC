4b098e7395f2a81976861b2383b1a8a17f7270bf
Sun Serega <latchenko3@yandex.ru>
2018-12-14 13:50:29 +0200

    Tester upgrade

--- src/Test.pas
+++ src/Test.pas
@@ -1,16 +1,10 @@
 ﻿uses StmParser;
 
 uses MiscData;
-
-//ToDo Проверить, не исправили ли issue компилятора
-// - #1520
+uses LocaleData;
 
 { $define SingleThread}
 { $define WriteDone}
-
-const
-  TimeToComp=2000;
-  TimeToExec=5000;
 
 
 
@@ -35,22 +29,29 @@ begin
 end;
 
 type
+  TesterException = class(Exception)
+    constructor(text: string) :=
+    inherited Create(text);
+  end;
+  
   Tester=abstract class
     
     curr_dir: string;
     sfn: string;
-    stg: Dictionary<string, string>;
-    exp_opt_code: string;
+    
     main_fname: string;
+    exp_opt_code: string;
+    exp_comp_err: string;
     
     
     
     function Copy: Tester; abstract;
     
-    procedure Test(dir: string); abstract;
-    
-    function StartTesting: boolean;
+    function StartTesting(dir: string): boolean;
     begin
+      curr_dir := dir;
+      sfn := dir+'\0.sactd';
+      
       Result := not System.IO.File.Exists(sfn);
       
       if Result then
@@ -80,10 +81,14 @@ type
       
     end;
     
-    procedure LoadSettings;
+    procedure Test(dir: string); abstract;
+    
+    
+    
+    function GetSettingsDict: Dictionary<string, string>;
     begin
       
-      stg := new Dictionary<string, string>;
+      Result := new Dictionary<string, string>;
       var lns := ReadAllLines(sfn);
       
       var i := -1;
@@ -114,205 +119,178 @@ type
         end;
         
         if not key.StartsWith(' ') then
-          stg.Add(key, sb.ToString.TrimEnd(#10));
+          Result.Add(key, sb.ToString.TrimEnd(#10));
       end;
       
-      if not stg.TryGetValue('#ExpOptCode', exp_opt_code) then exp_opt_code := nil;
-      if not stg.TryGetValue('#MainFName', main_fname) then main_fname := nil;
+    end;
+    
+    function LoadSettings: Dictionary<string, string>; virtual;
+    begin
+      Result := GetSettingsDict;
       
+      if not Result.TryGetValue('#MainFName', main_fname) then main_fname := 'Main.sac';
+      if not Result.TryGetValue('#ExpOptCode', exp_opt_code) then exp_opt_code := nil;
+      if not Result.TryGetValue('#ExpCompErr', exp_comp_err) then exp_comp_err := nil;
+      
+    end;
+    
+    
+    
+    function TemplatedCompile: Script;
+    const TimeToComp=2000;
+    begin
+      try
+        
+        var s: Script;
+        if TimedExecute(
+          ()->
+          begin
+            var ep := new ExecParams;
+            ep.SupprIO := true;
+            s := new Script(dir + '\' + main_fname, ep);
+          end,
+          TimeToComp
+        ) then
+          raise new TesterException($'{dir}: Error, compiling took too long!{#10}');
+        
+        var opt_code := s.ToString.Replace('#', '\#').TrimEnd(#10);
+        if exp_opt_code=nil then
+        begin
+          System.IO.File.AppendAllText(sfn, #10' #ExpOptCode'#10 + opt_code + #10);
+          exp_opt_code := opt_code;
+        end else
+        if opt_code <> exp_opt_code then
+          raise new TesterException($'{dir}: Error, wrong code!{#10}');
+        
+        if TimedExecute(
+          procedure->loop 10 do s.Optimize,
+          TimeToComp
+        ) then
+          raise new TesterException($'{dir}: Error, optimizing took too long!{#10}');
+        
+        opt_code := s.ToString.Replace('#', '\#').TrimEnd(#10);
+        if opt_code <> exp_opt_code then
+          raise new TesterException($'{dir}: Error, wrong code after optimize!{#10}');
+        
+        Result := s;
+        
+      except
+        on e: Exception do
+          if exp_comp_err=nil then
+            raise e else
+          if exp_comp_err='' then
+            System.IO.File.AppendAllText(sfn, #10' #ExpCompErr'#10 + e.Message + #10) else
+          if exp_comp_err<>e.Message then
+            raise new TesterException($'{dir}: Error, wrong error text!{#10}Exp: {exp_comp_err}{#10}Got: {e.Message}');
+      end;
     end;
     
   end;
   
-  ExecTester = class(Tester)
-    
-    exp_otp: string;
-    
-    
-    
-    function Copy: Tester; override := new ExecTester;
-    
-    procedure Test(dir: string := 'TestSuite\TestExec'); override :=
-    try
-      curr_dir := dir;
-      sfn := dir+'\0.sactd';
-      if StartTesting then exit;
-      
-      LoadSettings;
-      if not stg.TryGetValue('#ExpOtp', exp_otp) then exp_otp := nil;
-      
-      var s: Script;
-      if TimedExecute(
-        ()->
-        begin
-          var ep := new ExecParams;
-          ep.SupprIO := true;
-          s := new Script(dir + '\' + (main_fname=nil?'Main.sac':main_fname), ep);
-        end,
-        TimeToComp
-      ) then
-      begin
-        write($'{dir}: Error, compiling took too long!{#10}');
-        exit;
-      end;
-      
-      var opt_code := s.ToString.Replace('#', '\#').TrimEnd(#10);
-      if exp_opt_code=nil then
-      begin
-        System.IO.File.AppendAllText(sfn, #10' #ExpOptCode'#10 + opt_code + #10);
-        exp_opt_code := opt_code;
-      end else
-      if opt_code <> exp_opt_code then
-      begin
-        write($'{dir}: Error, wrong code!{#10}');
-        exit;
-      end;
-      
-      if TimedExecute(
-        procedure->begin loop 10 do s.Optimize end,//ToDo #1520
-        TimeToComp
-      ) then
-      begin
-        write($'{dir}: Error, optimizing took too long!{#10}');
-        exit;
-      end;
-      
-      opt_code := s.ToString.Replace('#', '\#').TrimEnd(#10);
-      if opt_code <> exp_opt_code then
-      begin
-        write($'{dir}: Error, wrong code after optimize!{#10}');
-        exit;
-      end;
-      
-      
-      
-      var otp := new StringBuilder;
-      s.otp := str->
-      begin
-        otp += str;
-        otp += #10;
-      end;
-      s.susp_called := procedure->otp += '%susp_called'#10;
-      s.stoped := procedure->otp += '%stoped'#10;
-      
-      if TimedExecute(procedure->s.Execute, TimeToExec) then
-        otp += '%aborted'#10;
-      
-      var otp_str := otp.ToString.TrimEnd(#10);
-      if exp_otp=nil then
-        System.IO.File.AppendAllText(sfn, #10' #ExpOtp'#10 + otp_str + #10) else
-      if otp_str <> exp_otp then
-      begin
-        write($'{dir}: Error, wrong output!{#10}');
-        exit;
-      end;
-      
-      {$ifdef WriteDone}
-      write($'DONE: {dir}{#10}');
-      {$endif WriteDone}
-      
-    except
-      on e: Exception do
-        writeln($'Exception in {dir}: {_ObjectToString(e)}');
-    end;
-    
-  end;
   CompTester = class(Tester)
     
     function Copy: Tester; override := new CompTester;
     
     procedure Test(dir: string := 'TestSuite\TestComp'); override :=
     try
-      curr_dir := dir;
-      sfn := dir+'\0.sactd';
-      if StartTesting then exit;
+      if StartTesting(dir) then exit;
       
       LoadSettings;
       
-      var s: Script;
-      if TimedExecute(
-        ()->
-        begin
-          var ep := new ExecParams;
-          ep.SupprIO := true;
-          s := new Script(dir + '\' + (main_fname=nil?'Main.sac':main_fname), ep);
-        end,
-        TimeToComp
-      ) then
-      begin
-        writeln($'{dir}: Error, compiling took too long!');
-        exit;
-      end;
-      
-      var opt_code := s.ToString.Replace('#', '\#').TrimEnd(#10);
-      if exp_opt_code=nil then
-      begin
-        System.IO.File.AppendAllText(sfn, #10' #ExpOptCode'#10 + opt_code + #10);
-        exp_opt_code := opt_code;
-      end else
-      if opt_code <> exp_opt_code then
-        writeln($'{dir}: Error, wrong code!');
-      
-      if TimedExecute(
-        procedure->begin loop 10 do s.Optimize end,//ToDo #1520
-        TimeToComp
-      ) then
-      begin
-        writeln($'{dir}: Error, optimizing took too long!');
-        exit;
-      end;
+      TemplatedCompile;
       
       {$ifdef WriteDone}
       write($'DONE: {dir}{#10}');
       {$endif WriteDone}
       
     except
-      on e: Exception do
-        writeln($'Exception in {dir}: {_ObjectToString(e)}');
+      on e: TesterException do writeln(e.Message);
+      on e: Exception do writeln($'Exception in {dir}: {_ObjectToString(e)}');
+    end;
+    
+  end;
+  ExecTester = class(Tester)
+    
+    exp_otp: string;
+    exp_exec_err: string;
+    
+    
+    
+    function Copy: Tester; override := new ExecTester;
+    
+    function LoadSettings: Dictionary<string, string>; override;
+    begin
+      Result := inherited LoadSettings;
+      
+      if not Result.TryGetValue('#ExpOtp', exp_otp) then exp_otp := nil;
+      if not Result.TryGetValue('#ExpExecErr', exp_exec_err) then exp_exec_err := nil;
+      
+    end;
+    
+    
+    procedure TemplatedExecute(s: Script);
+    const TimeToExec=5000;
+    begin
+      try
+        
+        var otp := new StringBuilder;
+        s.otp := str->
+        begin
+          otp += str;
+          otp += #10;
+        end;
+        s.susp_called := procedure->otp += '%susp_called'#10;
+        s.stoped := procedure->otp += '%stoped'#10;
+        
+        if TimedExecute(procedure->s.Execute, TimeToExec) then
+          otp += '%aborted'#10;
+        
+        var otp_str := otp.ToString.TrimEnd(#10);
+        if exp_otp=nil then
+          System.IO.File.AppendAllText(sfn, #10' #ExpOtp'#10 + otp_str + #10) else
+        if otp_str <> exp_otp then
+          raise new TesterException($'{dir}: Error, wrong output!{#10}');
+        
+      except
+        on e: Exception do
+          if exp_exec_err=nil then
+            raise e else
+          if exp_exec_err='' then
+            System.IO.File.AppendAllText(sfn, #10' #ExpExecErr'#10 + e.Message + #10) else
+          if exp_exec_err<>e.Message then
+            raise new TesterException($'{dir}: Error, wrong error text!{#10}Exp: {exp_exec_err}{#10}Got: {e.Message}');
+      end;
+    end;
+    
+    procedure Test(dir: string := 'TestSuite\TestExec'); override :=
+    try
+      if StartTesting(dir) then exit;
+      
+      LoadSettings;
+      
+      var s := TemplatedCompile;
+      TemplatedExecute(s);
+      
+      {$ifdef WriteDone}
+      write($'DONE: {dir}{#10}');
+      {$endif WriteDone}
+      
+    except
+      on e: TesterException do writeln(e.Message);
+      on e: Exception do writeln($'Exception in {dir}: {_ObjectToString(e)}');
     end;
     
   end;
 
-procedure TestErr(dir: string := 'TestErr');
-begin
-  if not System.IO.File.Exists(dir+'\0.sactd') then
-  begin
-    System.IO.Directory.EnumerateDirectories(dir).ForEach(TestErr);
-    exit;
-  end;
-  
-  
-  
-end;
-
-procedure SpecTests(dir: string := 'SpecialTests');
-begin
-  if not System.IO.File.Exists(dir+'\0.sactd') then
-  begin
-    System.IO.Directory.EnumerateDirectories(dir).ForEach(SpecTests);
-    exit;
-  end;
-  
-  
-  
-end;
-
 begin
   try
+    CurrLocale := LangList[0];
     
-    var c := 2;
-    var done := 0;
-    var done_lock := new object;
+    System.Threading.Tasks.Parallel.Invoke(
+      ExecTester.Create.Test,
+      CompTester.Create.Test
+    );
     
-    System.Threading.Thread.Create(()->begin ExecTester.Create.Test; lock done_lock do done += 1 end).Start;
-    System.Threading.Thread.Create(()->begin CompTester.Create.Test; lock done_lock do done += 1 end).Start;
-    
-//    TestErr;
-//    SpecTests;
-    
-    
-    
-    while done<c do Sleep(10);
     Writeln('Done testing');
     if not System.Console.IsOutputRedirected then readln;
     
